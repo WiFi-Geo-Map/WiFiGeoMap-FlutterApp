54681c61dba373444831b5b4d5cdb175282e41d2
Bhanu <bhanujarpula9@gmail.com>
2023-11-17 00:24:39 +0530

    done loading page

--- lib/Screens/loading_page.dart
+++ lib/Screens/loading_page.dart
@@ -1,41 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class LoadingPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:wifi_geo_map/utils/custom_sizedbox.dart';
+import 'package:wifi_geo_map/utils/custom_text.dart';
+
+class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  IconData currentIcon = Icons.wifi;
+  int iconIndex = 0;
+  List<IconData> iconList = [
+    Icons.wifi,
+    Icons.wifi_1_bar,
+    Icons.wifi_2_bar,
+    Icons.wifi_password
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    startAnimation();
+  }
+
+  void startAnimation() {
+    Timer.periodic(const Duration(milliseconds: 770), (timer) {
+      setState(() {
+        currentIcon = iconList[iconIndex];
+        iconIndex = (iconIndex + 1) % iconList.length;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.15,
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assests/images/background.png'),
+            fit: BoxFit.cover,
           ),
-          const SizedBox(
-            child: Center(
-              child: Text(
-                "Singing In",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 26, 98, 158),
-                    fontSize: 42,
-                    fontFamily: 'Poppins'),
+        ),
+        child: Column(children: [
+          const CustomSizedBox(height: 0.17, width: 1),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.1),
+            child: const CustomText(text: "Loading...", size: 50),
+          ),
+          const CustomSizedBox(height: 0.12, width: 1),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.1),
+            child: const CustomText(
+                text: "Please enable location services",
+                size: 28,
+                weight: FontWeight.w600),
+          ),
+          CustomSizedBox(
+            height: 0.40,
+            width: 1,
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              child: Icon(
+                currentIcon,
+                key: ValueKey<IconData>(currentIcon),
+                size: 100,
               ),
             ),
           ),
-          Image.asset(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.5,
-            'assets/Images/splash.gif',
-          ),
-          const SizedBox(
-            child: CircularProgressIndicator(
-              color: Color.fromARGB(255, 26, 98, 158),
-            ),
-          )
-        ],
+        ]),
       ),
     );
   }
